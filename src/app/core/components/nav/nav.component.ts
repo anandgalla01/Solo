5f244b8fde325234530f49711e03abf4b524cb08
Anand Galla <anandgalla01@gmail.com>
2024-05-14 20:00:35 +0000

    completed side navigation

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,22 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavigationEnd, NavigationStart, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent implements OnInit {
 
-  constructor(private route: Router) {
+  menuItems = [
+    { title: "Dashboard", url: "/dashboard", isActive: false },
+    { title: "Contacts", url: "/contacts", isActive: false },
+    { title: "Tasks", url: "/tasks", isActive: false },
+    { title: "Expenses", url: "/expenses", isActive: false },
+    { title: "Income", url: "/income", isActive: false },
+  ]
+  constructor(private router: Router) {
 
   }
 
   ngOnInit(): void {
-
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart)
+    ).subscribe((event: any) => {
+      this.menuItems.forEach((item) => item.isActive = item.url.includes(event["url"]));
+    });
   }
 
 }
